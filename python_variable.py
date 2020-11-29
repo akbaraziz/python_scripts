d0bd9aeafac332fb676e28250af61694bd6abbaf
Akbar Aziz <akbar.aziz@nutanix.com>
2020-11-29 00:25:49 -0600

    Updated Scripts

---
+++ python_variable.py
@@ -0,0 +1,7 @@
+oslist = ['Windows 2012R2', 'Windows 2016', 'Windows 2019']
+print ','.join(oslist)
+
+
+list_of_ints = [80, 443, 8080, 8081]
+>> > print str(list_of_ints).strip('[]')
+80, 443, 8080, 8081
